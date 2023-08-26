0cdc47126c7ff2e6fa05b2c9a7b8fac005ea5879
ikram3031 <mdikram.3031@gmail.com>
2023-08-26 14:27:55 +0600

    Registration Done

---
+++ app/register/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import RegisterForm from '@/components/Register';
+
+const register = () => {
+  return (
+    <div>
+        <RegisterForm />
+    </div>
+  )
+}
+
+export default register
